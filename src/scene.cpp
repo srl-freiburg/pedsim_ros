96a8d41aa0216ef55508e2ca812dd2f38cb64480
Billy Okal <okal.billy@googlemail.com>
2014-01-16 18:00:10 +0100

    reverting robot visual to collada model

--- src/scene.cpp
+++ src/scene.cpp
@@ -81,8 +81,8 @@ void Scene::runSimulation()
             robot_->setteleop(true);
     }
 
-    // ros::Rate r( 1 /  CONFIG.simh );
-    ros::Rate r( 15 ); // 15 Hz seems to be good visually
+    ros::Rate r( 1 /  CONFIG.simh );
+    // ros::Rate r( 15 ); // 15 Hz seems to be good visually
 
     while (ros::ok())
     {
@@ -253,26 +253,26 @@ void Scene::publishAgentVisuals()
 
         if (a->gettype() == robot_->gettype()) 
         {
-            // marker.type = visualization_msgs::Marker::MESH_RESOURCE;
-            // marker.mesh_resource = "package://simulator/images/darylbot.dae";
-            // marker.color.a = 1.0;
-            // marker.color.r = 1.0;
-            // marker.color.g = 1.0;
-            // marker.color.b = 1.0;
-
-            // marker.scale.x = 0.5;
-            // marker.scale.y = 0.5;
-            // marker.scale.z = 0.5;
-
-            marker.type = visualization_msgs::Marker::CYLINDER;
+            marker.type = visualization_msgs::Marker::MESH_RESOURCE;
+            marker.mesh_resource = "package://simulator/images/darylbot.dae";
             marker.color.a = 1.0;
-            marker.color.r = 0.0;
-            marker.color.g = 0.0;
+            marker.color.r = 1.0;
+            marker.color.g = 1.0;
             marker.color.b = 1.0;
 
-            marker.scale.x = 0.3;
-            marker.scale.y = 0.3;
-            marker.scale.z = 1.5;
+            marker.scale.x = 0.5;
+            marker.scale.y = 0.5;
+            marker.scale.z = 0.5;
+
+            // marker.type = visualization_msgs::Marker::CYLINDER;
+            // marker.color.a = 1.0;
+            // marker.color.r = 0.0;
+            // marker.color.g = 0.0;
+            // marker.color.b = 1.0;
+
+            // marker.scale.x = 0.3;
+            // marker.scale.y = 0.3;
+            // marker.scale.z = 1.5;
         }
         else
         {
