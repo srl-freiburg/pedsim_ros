0172eaf504f2228344003d0ddf723ce087833b32
Billy Okal <okal.billy@googlemail.com>
2014-03-06 14:35:28 +0100

    adding more vector utils from Sven implementation

--- pedsim_simulator/3rdparty/libpedsim/ped_vector.h
+++ pedsim_simulator/3rdparty/libpedsim/ped_vector.h
@@ -12,6 +12,11 @@
 #define LIBEXPORT
 #endif
 
+
+#include "ped_angle.h"
+#include <cmath>
+
+
 namespace Ped {
 
 /// Vector helper class. This is basically a struct with some related functions attached.
@@ -21,51 +26,64 @@ namespace Ped {
 class LIBEXPORT Tvector {
 public:
 
-    // Defaul constructor
+   // Default constructor
     Tvector();
-
+    
     // Initializing constructor
-    Tvector(double px, double py, double pz) : x(px), y(py), z(pz) {};
-
-    // Copy constructor
-    Tvector(const Tvector& source) : x(source.x), y(source.y), z(source.z) {};
-
-    // Assignment operator
-    Tvector& operator= (const Tvector& source) {
-        if (this == &source) return *this;
-        x = source.x;
-        y = source.y;
-        z = source.z;
-        return *this;
-    }
-
-    Tvector& operator+ (const Tvector& source) {
-        x += source.x;
-        y += source.y;
-        z += source.z;
-        return *this;
-    }
-
-    Tvector& operator- (const Tvector& source) {
-        x -= source.x;
-        y -= source.y;
-        z -= source.z;
-        return *this;
-    }
-
-    virtual ~Tvector();
+    Tvector(double px, double py, double pz = 0) : x(px), y(py), z(pz) {};
 
 
+    // Static Methods
+    static Tvector fromPolar(const Tangle& angleIn, double radiusIn = 1);
+
+
+    // Methods
+    double length() const;
+    double lengthSquared() const;
+    void normalize();
+    Tvector normalized() const;
+    void scale(double factor);
+    Tvector scaled(double factor) const;
+
+    Tvector leftNormalVector() const;
+    Tvector rightNormalVector() const;
+
+    double polarRadius() const;
+    Tangle polarAngle() const;
+
+    Tangle angleTo(const Tvector &other) const;
+
+    static double scalar(const Tvector &a, const Tvector &b);
+    static double dotProduct(const Tvector &a, const Tvector &b);
+    static Tvector crossProduct(const Tvector &a, const Tvector &b);
+
+    // Operators
+    Tvector operator+(const Tvector& other) const;
+    Tvector operator-(const Tvector& other) const;
+    Tvector operator*(double factor) const;
+    Tvector operator/(double divisor) const;
+    Tvector& operator+=(const Tvector& vectorIn);
+    Tvector& operator-=(const Tvector& vectorIn);
+    Tvector& operator*=(double factor);
+    Tvector& operator*=(const Tvector& vectorIn);
+    Tvector& operator/=(double divisor);
+
+    // Attributes
     double x;
     double y;
     double z;
+
     void cross(const Tvector &a, const Tvector &b);
     friend double scalar(const Tvector &a, const Tvector &b);
-    void normalize();
 };
 
 
 }
 
+
+bool operator==(const Ped::Tvector& vector1In, const Ped::Tvector& vector2In);
+bool operator!=(const Ped::Tvector& vector1In, const Ped::Tvector& vector2In);
+Ped::Tvector operator-(const Ped::Tvector& vectorIn);
+Ped::Tvector operator*(double factor, const Ped::Tvector& vector);
 
 #endif
