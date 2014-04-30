10a59d049f52eeb7e924e3955b15733b9308388e
Billy Okal <okal.billy@googlemail.com>
2014-04-30 15:39:17 +0200

    new scenerios for collecting training data

--- pedsim_simulator/src/config.cpp
+++ pedsim_simulator/src/config.cpp
@@ -69,7 +69,7 @@ Config::Config ( QObject* parent )
     robot_mode = SOCIAL_DRIVE;
     robot_wait_time = 100;
 
-	groups_enabled = true;
+	groups_enabled = false;
 
 
     // distribution parameters
