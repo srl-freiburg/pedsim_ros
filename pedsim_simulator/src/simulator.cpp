7015f947eaefa6ad711880fcebcfa724fdf531fe
Billy Okal <okal.billy@googlemail.com>
2014-07-01 14:35:38 +0200

    minor mods recording video

--- pedsim_simulator/src/simulator.cpp
+++ pedsim_simulator/src/simulator.cpp
@@ -421,9 +421,9 @@ void Simulator::publishGroupVisuals()
             marker.id = m->getId() +1000;
 
             marker.color.a = 0.7;
-            marker.color.r = 0.0;
-            marker.color.g = 0.0;
-            marker.color.b = 1.0;
+            marker.color.r = 1.0;
+            marker.color.g = 1.0;
+            marker.color.b = 0.0;
 
             marker.scale.x = 0.1;
             marker.scale.y = 0.1;
