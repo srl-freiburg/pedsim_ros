04fe890235b10f0c3c838f58fe8419d80200e0a4
silgon <silgon3200@gmail.com>
2014-11-24 11:33:03 +0100

    - teleop working as virtually a real robot, command in x and theta of the robot representation - old file for teleop in velocities x and y of the environment representation now called "robot_teleop_joystick_vxy.cpp"

--- pedsim_simulator/src/robot_teleop_joystick_vxy.cpp
+++ pedsim_simulator/src/robot_teleop_joystick_vxy.cpp
@@ -44,34 +44,31 @@ private:
     void joyCallback(const sensor_msgs::Joy::ConstPtr& joy);
     void publish();
     ros::NodeHandle ph_, nh_;
-    int linear_, angular_, deadman_axis_;
-    double a_current;
-    double l_scale_, a_scale_;
+    int linear_x, linear_y, deadman_axis_;
+    double x_scale_, y_scale_;
     ros::Publisher vel_pub_;
     ros::Subscriber joy_sub_;
-    pedsim_msgs::AgentState vel;
+    pedsim_msgs::AgentState last_published_;
     boost::mutex publish_mutex_;
     bool deadman_pressed_;
     bool zero_twist_published_;
     ros::Timer timer_;
     double frequency;
-    double joy_x, joy_th;
 };
 JoyTeleop::JoyTeleop():
     ph_("~"),
-    linear_(1),
-    angular_(2),
+    linear_x(2),
+    linear_y(3),
     deadman_axis_(4),
-    a_current(0),
-    l_scale_(1),
-    a_scale_(1),
-    frequency(10)
+    x_scale_(1),
+    y_scale_(1),
+    frequency(20)
 {
-    ph_.param("axis_linear", linear_, linear_);
-    ph_.param("axis_angular", angular_, angular_);
+    ph_.param("axis_linear_x", linear_x, linear_x);
+    ph_.param("axis_linear_y", linear_y, linear_y);
     ph_.param("axis_deadman", deadman_axis_, deadman_axis_);
-    ph_.param("scale_angular", a_scale_, a_scale_);
-    ph_.param("scale_linear", l_scale_, l_scale_);
+    ph_.param("scale_angular", y_scale_, y_scale_);
+    ph_.param("scale_linear", x_scale_, x_scale_);
     deadman_pressed_ = false;
     zero_twist_published_ = false;
     vel_pub_ = ph_.advertise<pedsim_msgs::AgentState>("/pedsim/robot_command", 1, true);
@@ -81,23 +78,22 @@ JoyTeleop::JoyTeleop():
 void JoyTeleop::joyCallback(const sensor_msgs::Joy::ConstPtr& joy) {
     pedsim_msgs::AgentState vel;
     deadman_pressed_ = joy->buttons[deadman_axis_];
-    joy_x = joy->axes[linear_];
-    joy_th = joy->axes[angular_];
-}
-void JoyTeleop::publish() {
-    boost::mutex::scoped_lock lock(publish_mutex_);
     vel.type = 2;
     if (deadman_pressed_) {
-        a_current += joy_th*a_scale_/frequency;
-        vel.twist.linear.x = l_scale_*joy_x*cos(a_current);
-        vel.twist.linear.y = l_scale_*joy_x*sin(a_current);
+        vel.twist.linear.x = -x_scale_*joy->axes[linear_x];
+        vel.twist.linear.y = y_scale_*joy->axes[linear_y];
+        last_published_ = vel;
         zero_twist_published_ = false;
     } else if (!deadman_pressed_ && !zero_twist_published_) {
         vel.twist.linear.x = 0;
         vel.twist.linear.y = 0;
+        last_published_ = vel;
         zero_twist_published_ = true;
     }
-    vel_pub_.publish(vel);
+}
+void JoyTeleop::publish() {
+    boost::mutex::scoped_lock lock(publish_mutex_);
+    vel_pub_.publish(last_published_);
 }
 int main(int argc, char** argv) {
     ros::init(argc, argv, "robot_teleop_joy");
