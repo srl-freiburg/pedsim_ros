307b58c293f9410c7905fefd8c0661c7d9953779
Billy Okal <okal.billy@gmail.com>
2016-07-03 02:48:39 +0200

    ENH undo style mixup from last commit

--- pedsim/include/pedsim/ped_vector.h
+++ pedsim/include/pedsim/ped_vector.h
@@ -23,57 +23,60 @@ namespace Ped {
 /// \date    2010-02-12
 class LIBEXPORT Tvector {
 public:
-  // Default constructor
-  Tvector();
+    // Default constructor
+    Tvector();
 
-  // Initializing constructor
-  Tvector(double px, double py, double pz = 0) : x(px), y(py), z(pz){};
+    // Initializing constructor
+    Tvector(double px, double py, double pz = 0)
+        : x(px)
+        , y(py)
+        , z(pz){};
 
-  // Static Methods
-  static Tvector fromPolar(const Tangle &angleIn, double radiusIn = 1);
+    // Static Methods
+    static Tvector fromPolar(const Tangle& angleIn, double radiusIn = 1);
 
-  // Methods
-  bool isValid() const;
-  double length() const;
-  double lengthSquared() const;
-  void normalize();
-  Tvector normalized() const;
-  void scale(double factor);
-  Tvector scaled(double factor) const;
+    // Methods
+    bool isValid() const;
+    double length() const;
+    double lengthSquared() const;
+    void normalize();
+    Tvector normalized() const;
+    void scale(double factor);
+    Tvector scaled(double factor) const;
 
-  Tvector leftNormalVector() const;
-  Tvector rightNormalVector() const;
+    Tvector leftNormalVector() const;
+    Tvector rightNormalVector() const;
 
-  double polarRadius() const;
-  Tangle polarAngle() const;
+    double polarRadius() const;
+    Tangle polarAngle() const;
 
-  Tangle angleTo(const Tvector &other) const;
+    Tangle angleTo(const Tvector& other) const;
 
-  static double scalar(const Tvector &a, const Tvector &b);
-  static double dotProduct(const Tvector &a, const Tvector &b);
-  static Tvector crossProduct(const Tvector &a, const Tvector &b);
+    static double scalar(const Tvector& a, const Tvector& b);
+    static double dotProduct(const Tvector& a, const Tvector& b);
+    static Tvector crossProduct(const Tvector& a, const Tvector& b);
 
-  // Operators
-  Tvector operator+(const Tvector &other) const;
-  Tvector operator-(const Tvector &other) const;
-  Tvector operator*(double factor) const;
-  Tvector operator/(double divisor) const;
-  Tvector &operator+=(const Tvector &vectorIn);
-  Tvector &operator-=(const Tvector &vectorIn);
-  Tvector &operator*=(double factor);
-  Tvector &operator*=(const Tvector &vectorIn);
-  Tvector &operator/=(double divisor);
+    // Operators
+    Tvector operator+(const Tvector& other) const;
+    Tvector operator-(const Tvector& other) const;
+    Tvector operator*(double factor) const;
+    Tvector operator/(double divisor) const;
+    Tvector& operator+=(const Tvector& vectorIn);
+    Tvector& operator-=(const Tvector& vectorIn);
+    Tvector& operator*=(double factor);
+    Tvector& operator*=(const Tvector& vectorIn);
+    Tvector& operator/=(double divisor);
 
-  // Attributes
-  double x;
-  double y;
-  double z;
+    // Attributes
+    double x;
+    double y;
+    double z;
 };
 }
 
-bool operator==(const Ped::Tvector &vector1In, const Ped::Tvector &vector2In);
-bool operator!=(const Ped::Tvector &vector1In, const Ped::Tvector &vector2In);
-Ped::Tvector operator-(const Ped::Tvector &vectorIn);
-Ped::Tvector operator*(double factor, const Ped::Tvector &vector);
+bool operator==(const Ped::Tvector& vector1In, const Ped::Tvector& vector2In);
+bool operator!=(const Ped::Tvector& vector1In, const Ped::Tvector& vector2In);
+Ped::Tvector operator-(const Ped::Tvector& vectorIn);
+Ped::Tvector operator*(double factor, const Ped::Tvector& vector);
 
 #endif
