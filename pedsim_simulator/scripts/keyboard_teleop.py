6c43ca944d17c67a87a1f7aa1fa7035a93fd3877
Billy <okal.billy@gmail.com>
2018-10-31 15:01:01 -0500

    Utilize sim factor and update rate
    - Load user set simulation factor and update rate in the main sim process
    - Adapt simulated robot controller to use the right paramters
    - Minor formatting of KBD teleop code.

--- pedsim_simulator/scripts/keyboard_teleop.py
+++ pedsim_simulator/scripts/keyboard_teleop.py
@@ -34,7 +34,10 @@ import rospy
 
 from geometry_msgs.msg import Twist
 
-import sys, select, termios, tty
+import sys
+import select
+import termios
+import tty
 
 msg = """
 Control the PedSim robot!
@@ -54,24 +57,25 @@ CTRL-C to quit
 """
 
 moveBindings = {
-        'i':(1,0),
-        'o':(1,-1),
-        'j':(0,1),
-        'l':(0,-1),
-        'u':(1,1),
-        ',':(-1,0),
-        '.':(-1,1),
-        'm':(-1,-1),
-           }
+    'i': (1, 0),
+    'o': (1, -1),
+    'j': (0, 1),
+    'l': (0, -1),
+    'u': (1, 1),
+    ',': (-1, 0),
+    '.': (-1, 1),
+    'm': (-1, -1),
+}
 
-speedBindings={
-        'q':(1.1,1.1),
-        'z':(.9,.9),
-        'w':(1.1,1),
-        'x':(.9,1),
-        'e':(1,1.1),
-        'c':(1,.9),
-          }
+speedBindings = {
+    'q': (1.1, 1.1),
+    'z': (.9, .9),
+    'w': (1.1, 1),
+    'x': (.9, 1),
+    'e': (1, 1.1),
+    'c': (1, .9),
+}
+
 
 def getKey():
     tty.setraw(sys.stdin.fileno())
@@ -84,13 +88,16 @@ def getKey():
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
     return key
 
+
 speed = .2
 turn = 1
 
-def vels(speed,turn):
-    return "currently:\tspeed %s\tturn %s " % (speed,turn)
 
-if __name__=="__main__":
+def vels(speed, turn):
+    return "currently:\tspeed %s\tturn %s " % (speed, turn)
+
+
+if __name__ == "__main__":
     settings = termios.tcgetattr(sys.stdin)
 
     rospy.init_node('pedsim_keyboard_teleop')
@@ -107,7 +114,7 @@ if __name__=="__main__":
     control_turn = 0
     try:
         print msg
-        print vels(speed,turn)
+        print vels(speed, turn)
         while(1):
             key = getKey()
             if key in moveBindings.keys():
@@ -119,11 +126,11 @@ if __name__=="__main__":
                 turn = turn * speedBindings[key][1]
                 count = 0
 
-                print vels(speed,turn)
+                print vels(speed, turn)
                 if (status == 14):
                     print msg
                 status = (status + 1) % 15
-            elif key == ' ' or key == 'k' :
+            elif key == ' ' or key == 'k':
                 x = 0
                 th = 0
                 control_speed = 0
@@ -140,22 +147,26 @@ if __name__=="__main__":
             target_turn = turn * th
 
             if target_speed > control_speed:
-                control_speed = min( target_speed, control_speed + 0.02 )
+                control_speed = min(target_speed, control_speed + 0.02)
             elif target_speed < control_speed:
-                control_speed = max( target_speed, control_speed - 0.02 )
+                control_speed = max(target_speed, control_speed - 0.02)
             else:
                 control_speed = target_speed
 
             if target_turn > control_turn:
-                control_turn = min( target_turn, control_turn + 0.1 )
+                control_turn = min(target_turn, control_turn + 0.1)
             elif target_turn < control_turn:
-                control_turn = max( target_turn, control_turn - 0.1 )
+                control_turn = max(target_turn, control_turn - 0.1)
             else:
                 control_turn = target_turn
 
             twist = Twist()
-            twist.linear.x = control_speed; twist.linear.y = 0; twist.linear.z = 0
-            twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = control_turn
+            twist.linear.x = control_speed
+            twist.linear.y = 0
+            twist.linear.z = 0
+            twist.angular.x = 0
+            twist.angular.y = 0
+            twist.angular.z = control_turn
             pub.publish(twist)
 
     except:
@@ -163,8 +174,12 @@ if __name__=="__main__":
 
     finally:
         twist = Twist()
-        twist.linear.x = 0; twist.linear.y = 0; twist.linear.z = 0
-        twist.angular.x = 0; twist.angular.y = 0; twist.angular.z = 0
+        twist.linear.x = 0
+        twist.linear.y = 0
+        twist.linear.z = 0
+        twist.angular.x = 0
+        twist.angular.y = 0
+        twist.angular.z = 0
         pub.publish(twist)
 
     termios.tcsetattr(sys.stdin, termios.TCSADRAIN, settings)
