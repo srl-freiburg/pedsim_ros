f8b434e41bcce03ff91cdc0372b6677b4f8c7fbb
mahmoud-a-ali <46086657+mahmoud-a-ali@users.noreply.github.com>
2019-12-05 02:05:36 +0100

    integration of pedsim_simulator into Gazebo (#43)
    * add pedsim_gazebo_plugin pkg
    
    * clean cmake/pkg.xml files
    
    * modify readme file

---
+++ pedsim_gazebo_plugin/README.md
@@ -0,0 +1,22 @@
+# pedsim_gazebo_plugin package
+
+This package integrates the Pedestrian Simulator pedsim_simulator into Gazebo.
+
+### Features
+- it converts pedsim scenarios to gazebo worlds 
+- it spawns pedsim agents into gazebo  
+- it continously updates the poses of the spawned agents
+
+### Sample usage
+#### Step 1. Generate gazebo world + launch file for the corresponding pedsim_scenario (scenario_file should be in `pedsim_simulator/scenarios/` directory)
+```
+$ rosrun pedsim_gazebo_plugin spawn_pedsim_agents.py
+```
+#### Step 2. After running the pedsim simulator, use the launch file generated from step 1   
+```
+$ roslaunch pedsim_gazebo_plugin <scenario_name>.launch
+```
+#### Step 3. To synchronize your own robot with the diff_robot that is included in the pedsim package: 
+##### 1. Spawn your own robot (diff_drive_robot), consider the same coordinates in the scenario file.
+##### 2. Remap `/pedbot/control/cmd_vel` to the topic you are using to control your own robot 
+
---
+++ pedsim_gazebo_plugin/scripts/spawn_pedsim_agents.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+"""
+Created on Mon Dec  2 17:03:34 2019
+
+@author: mahmoud
+"""
+
+import rospy
+from gazebo_msgs.srv import SpawnModel
+from geometry_msgs.msg import *
+from rospkg import RosPack
+from pedsim_msgs.msg  import AgentStates
+
+# xml file containing a gazebo model to represent agent, currently is represented by a cubic but can be changed
+global xml_file
+
+def actor_poses_callback(actors):
+    for actor in actors.agent_states:
+        actor_id = str( actor.id )
+        actor_pose = actor.pose
+        rospy.loginfo("Spawning model: actor_id = %s", actor_id)
+
+        model_pose = Pose(Point(x= actor_pose.position.x,
+                               y= actor_pose.position.y,
+                               z= actor_pose.position.z),
+                         Quaternion(actor_pose.orientation.x,
+                                    actor_pose.orientation.y,
+                                    actor_pose.orientation.z,
+                                    actor_pose.orientation.w) )
+
+        spawn_model(actor_id, xml_string, "", model_pose, "world")
+    rospy.signal_shutdown("all agents have been spawned !")
+
+
+
+
+if __name__ == '__main__':
+
+    rospy.init_node("spawn_pedsim_agents")
+    
+    rospack1 = RosPack()
+    pkg_path = rospack1.get_path('pedsim_gazebo_plugin')
+    file_xml = open(pkg_path + "/models/actor_model.sdf")
+    xml_string = file_xml.read()
+
+    print("Waiting for gazebo services...")
+    rospy.wait_for_service("gazebo/spawn_sdf_model")
+    spawn_model = rospy.ServiceProxy("gazebo/spawn_sdf_model", SpawnModel)
+    print("service: spawn_sdf_model is available ....")
+    rospy.Subscriber("/pedsim_simulator/simulated_agents", AgentStates, actor_poses_callback)
+
+    rospy.spin()
